8680332bf24c8e0537fc4e84634efec59be25353
colocolov <colocolov.stas@gmail.com>
2022-06-23 16:01:42 +0300

    Add lesson 16

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+class First {
+  hello() {
+    console.log(`Привет я метод родителя!`);
+  }
+}
+
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log(`А я наследуемый метод!`);
+  }
+}
+
+const hello = new Second();
+hello.hello();
